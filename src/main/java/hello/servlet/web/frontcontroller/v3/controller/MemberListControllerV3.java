b4c680dc485670e39ad3b0cff2cda1811e9fa31d
sussa3007 <sussa1933@gmail.com>
2022-10-23 14:16:30 +0900

    Update #2 - View Model V3
    # View 기능 모델화 하여 구분
    # 컨트롤러의 서블릿 종속성 제거

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.Modelview;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+
+    @Override
+    public Modelview process(Map<String, String> paramMap) {
+        List<Member> members = memberRepository.findAll();
+
+        Modelview mv = new Modelview("members");
+        mv.getModel().put("members",members);
+
+        return mv;
+    }
+}
